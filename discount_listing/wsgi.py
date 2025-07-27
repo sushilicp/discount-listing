49cef188a404c019376bdaaa9e68ccc8de369eee
sushilicp <sushil.parajuli@icp.edu.np>
2025-07-27 21:11:46 +0545

    added wsgi 0-

--- discount_listing/wsgi.py
+++ discount_listing/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discount-listing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discount_listing.settings')
 
 application = get_wsgi_application()
